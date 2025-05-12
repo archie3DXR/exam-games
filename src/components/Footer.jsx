5ffebf6caca9e54579614467995cdf3509ee494f
Archana Majumdar <archie.majumdar@gmail.com>
2025-05-12 17:24:04 +1000

    clear for tutorial

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default () => (
-	<footer
-		className='h-[50px] w-full bg-[#999999]
-                     flex items-center justify-center'
-	>
-		<span className='text-white text-sm'> [ footer ] </span>
-	</footer>
-)
--- src/engine/useKeyPress.js
+++
@@ -1,7 +0,0 @@
-import { useEffect } from 'react';
-export const useKeyPress = (handler) => {
-  useEffect(() => {
-    window.addEventListener('keydown', handler);
-    return () => window.removeEventListener('keydown', handler);
-  }, [handler]);
-};
